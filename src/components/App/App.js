cda95c3cc7837697a3d0ceb4904d146bda1774a2
Doaa Mahely <dmahely@hotmail.com>
2020-08-28 22:19:59 +0400

    Add number of rounds to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,13 @@ import { Start } from '../Start/Start';
 import { RoundSelection } from '../RoundSelection/RoundSelection';
 
 const App = () => {
-  const [route, setRoute] = useState('roundSelection');
+  const [route, setRoute] = useState('start');
+  const [rounds, setRounds] = useState(5);
 
     return (
       <div className="App--container">
         {route === 'start' && <Start setRoute={setRoute} />}
-        {route === 'roundSelection' && <RoundSelection setRoute={setRoute} />}
+        {route === 'roundSelection' && <RoundSelection setRoute={setRoute} setRounds={setRounds} />}
       </div>
     );
   }
--- src/components/RoundSelection/RoundSelection.js
+++ src/components/RoundSelection/RoundSelection.js
@@ -3,16 +3,16 @@ import './RoundSelection.css';
 import { Option } from '../Option/Option';
 
 const RoundSelection = (props) => {
-    const {setRoute} = props;
+    const {setRoute, setRounds} = props;
     
     return (
         <div className="RoundSelection--container">
             <span className="RoundSelection--question">How many rounds do you want to play?</span>
             <div className="RoundSelection--options-container">
-                <Option text="5" color="yellow" onClick={() => setRoute('play')} />
-                <Option text="10" color="pink" onClick={() => setRoute('play')} />
-                <Option text="15" color="green" onClick={() => setRoute('play')} />
-                <Option text="20" color="navy" onClick={() => setRoute('play')} />
+                <Option text="5" color="yellow" onClick={() => { setRoute('play'); setRounds(5) }} />
+                <Option text="10" color="pink" onClick={() => { setRoute('play'); setRounds(10) }} />
+                <Option text="15" color="green" onClick={() => { setRoute('play'); setRounds(15) }} />
+                <Option text="20" color="navy" onClick={() => { setRoute('play'); setRounds(20) }} />
             </div>
         </div>
     );
