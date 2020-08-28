43ffe8bdd4bc639dd6c13bbdab214998a7f905f3
Doaa Mahely <dmahely@hotmail.com>
2020-08-29 01:49:43 +0400

    Add result component with styling

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Result.css'
+import { Button } from '../Button/Button';
 
 const Result = (props) => {
     return (
         <div className="Result--container">
-            <p className="Result--result">{props.result}</p>
-            <p className="Result--payload">{props.payload}</p>
+            <img className="Result--album-image" src="https://images-na.ssl-images-amazon.com/images/I/71uR2KhRrZL._AC_SL1066_.jpg" alt="Album name" />
+            <p className="Result--result">{props.result || "Correct"}</p>
+            <p className="Result--payload">{props.payload || "Beyoncé is also the artist behind Lemonade (2016) and BEYONCÉ (2013)"}</p>
+            <Button text="Continue" />
         </div>
         )
     }
