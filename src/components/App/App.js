38764aaaecd10400552655755abc635ac886fa38
Doaa Mahely <dmahely@hotmail.com>
2020-09-02 00:39:24 +0400

    Add fetching access token and saving it to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Start } from '../Start/Start';
 import { RoundSelection } from '../RoundSelection/RoundSelection';
 import { Round } from '../Round/Round';
 import { Result } from '../Result/Result';
 
+const baseURL = process.env.REACT_APP_SPOTIFY_BASE_URL,
+  apiTokenURL = process.env.REACT_APP_SPOTIFY_ACCESS_TOKEN_URL,
+  client_id = process.env.REACT_APP_CLIENT_ID,
+  client_secret = process.env.REACT_APP_CLIENT_SECRET;
+
 const App = () => {
   const [route, setRoute] = useState('start');
   const [rounds, setRounds] = useState(5);
+  const [accessToken, setAccessToken] = 
+  useState({
+    token: null,
+    expires_at: null
+  });
+
+  useEffect(() => {
+    const fetchDataAccessToken = async() => {
+
+      // encodes client id and secret to base64
+      const authParam = btoa(client_id + ':' + client_secret);
+      const token = await fetch(apiTokenURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + authParam 
+        },
+        body: new URLSearchParams({
+          grant_type: 'client_credentials'
+        })
+      })
+      .then(response => response.json())
+      .then(data => data.access_token)
+      .catch(err => console.log(err));
+
+      // calculate expiry date and time (current time + one hour)
+      const expires_at = Date.now() + (60 * 60 * 1000);
+
+      setAccessToken({ token, expires_at });
+    }
+
+    fetchDataAccessToken();
+  }, []);
 
     return (
       <div className="App--container">
