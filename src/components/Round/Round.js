3891ecbdab033d1ace81f3a9a62504066f180332
Doaa Mahely <dmahely@hotmail.com>
2020-09-07 00:11:56 +0400

    Set round album cover dynamically

--- src/components/Round/Round.js
+++ src/components/Round/Round.js
@@ -4,7 +4,7 @@ import { Progress } from '../Progress/Progress.js';
 import { Artist } from '../Artist/Artist.js';
 
 const Round = (props) => {
-    const {setRoute, rounds, setCurrentRound, currentRound} = props;
+    const {setRoute, rounds, setCurrentRound, currentRound, albums} = props;
 
     useEffect(() => {
         setCurrentRound(currentRound => currentRound + 1);
@@ -13,10 +13,13 @@ const Round = (props) => {
     // calculate current progress
     const width = (currentRound / rounds) * 100;
 
+    const albumCoverArt = albums[currentRound].coverArt;
+    const albumName = albums[currentRound].name;
+
     return (
         <div className="Round--container">
             <Progress width={width} currentRound={currentRound} rounds={rounds} />
-            <img className="Round--album-image" src="https://images-na.ssl-images-amazon.com/images/I/71uR2KhRrZL._AC_SL1066_.jpg" alt="Album name" />
+            <img className="Round--album-image" src={albumCoverArt} alt={albumName} />
             <div className="Round--artists-container">
                 <Artist number="1" name="Teyana Taylor" image="https://www.rollingstone.com/wp-content/uploads/2020/06/teyana-taylor.jpg" setRoute={setRoute}/>
                 <Artist number="2" name="Beyoncé" image="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU0MTU0MTc3NzkzMTA3MzU1/beyonce-attends-tidal-x-1015-on-october-15-2016-in-new-york-city-photo-by-theo-wargogetty-images-for-tidal-sqaure.jpg" setRoute={setRoute}/>
