97e4247efd1d238bf9ceab8844f18217ec9a8e37
Doaa Mahely <dmahely@hotmail.com>
2020-09-09 00:10:36 +0400

    Check for current token validity before fetching new token

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,29 +6,35 @@ import { Round } from '../Round/Round';
 import { Result } from '../Result/Result';
 import { AccessTokenContext } from '../../hooks/TokenContext';
 import { fetchAccessToken } from '../fetchAccessToken';
+import { isAccessTokenValid } from '../isAccessTokenValid';
 
 const App = () => {
   const [route, setRoute] = useState('start');
   const [rounds, setRounds] = useState(5);
-  const [accessToken, setAccessToken] = 
-  useState({
-    token: null,
-    expires_at: null
-  });
   const [albums, setAlbums] = useState({albums: []});
   const [currentRound, setCurrentRound] = useState(0);
+
+  const accessToken = {
+    token: null,
+    expires_at: null
+  };
 
   useEffect(() => {
     // get access token and save it in localStorage
     
     const getToken = async() => {
-      const token = await fetchAccessToken();
-      localStorage.setItem('token', JSON.stringify(token));
+      
+      if(!await isAccessTokenValid()) {
+        const token = await fetchAccessToken();
+        localStorage.setItem('token', JSON.stringify(token.token));
+        localStorage.setItem('expiry', JSON.stringify(token.expires_at));
+      }
+      
     }
-
     getToken();
+
     
-  }, []);
+  }, [accessToken]);
 
     return (
       <div className="App--container">
