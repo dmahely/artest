1bb31f80f051dbb23c82f45a7260117d0af841db
Doaa Mahely <dmahely@hotmail.com>
2020-09-12 01:48:21 +0400

    Add tweet button with functionality

--- src/components/FinalResult/FinalResult.js
+++ src/components/FinalResult/FinalResult.js
@@ -4,24 +4,35 @@ import scoreBackground from '../../assets/images/score-background.svg';
 import { Button } from '../Button/Button';
 import { getFinalScore } from '../getFinalScore';
 import { getAlbumResults } from '../getAlbumResults';
+import { ShareButton } from '../ShareButton/ShareButton';
+import twitterLogo from '../../assets/images/twitter-logo.svg';
+
+const twitterBaseURL = 'https://twitter.com/intent/tweet';
 
 const FinalResult = (props) => {
     const { setRoute, results, albums } = props;
 
     const score = getFinalScore(results);
     const albumResults = getAlbumResults(albums, results);
+
+    const handleTweetShare = () => {
+        console.log(twitterBaseURL);
+        const tweet = `I got ${score} playing Artest! What about you? Check out the game on https://dmahely.github.io/artest/`;
+        window.open(`${twitterBaseURL}?text=${tweet}`);
+    }
     
     return (
         <div className="FinalResult--container">
             <div className="FinalResult--albums">
-                {albumResults.map(album => {
+                {albumResults.map((album, i) => {
                     // todo: add FinalResult--album-cover className to this element
-                    return <img src={album.cover} alt="Album cover" className={album.result ? 'correct' : 'incorrect'}/>
+                    return <img key={i} src={album.cover} alt="Album cover" className={album.result ? 'correct' : 'incorrect'}/>
                 })}
             </div>
             <img src={scoreBackground} alt="background"/>
             <p>{score}</p>
-            <Button text="Play again" onClick={() => setRoute('start')}/>
+            <Button text="Play again" onClick={() => setRoute('start')} />
+            <ShareButton icon={twitterLogo} onClick={handleTweetShare} />
         </div>
     );
 }
---
+++ src/components/ShareButton/ShareButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ShareButton.css'
+
+const ShareButton = (props) => {
+    return (
+        <button className="ShareButton--button" onClick={props.onClick}>
+            <img className="ShareButton--icon" src={props.icon} alt={props.icon} />
+        </button>
+    );
+}
+
+export { ShareButton };
