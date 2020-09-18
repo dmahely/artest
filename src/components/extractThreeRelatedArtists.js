4fa7ac78611f3c3e2af43e1dee10ddb1e8bb20dd
Doaa Mahely <dmahely@hotmail.com>
2020-09-18 21:43:47 +0400

    Add getting random artists when no related artists are found

--- src/components/extractThreeRelatedArtists.js
+++ src/components/extractThreeRelatedArtists.js
@@ -1,3 +1,5 @@
+import { artists } from '../components/artists';
+
 const extractThreeRelatedArtists = (data) => {
     const relatedArtists = data.artists.slice(0, 3).map((artist) => {
         const artistObj = {
@@ -8,6 +10,15 @@ const extractThreeRelatedArtists = (data) => {
         };
         return artistObj;
     });
+
+    // if there are less than 3 related artists, get a
+    // random artist from fallback array and add it as an option
+    while(relatedArtists.length < 3) {
+        const randomArtist =
+            artists[Math.floor(Math.random() * artists.length)];
+        const randomIndex = Math.floor(Math.random() * relatedArtists.length);
+        relatedArtists.splice(randomIndex, 0, randomArtist);
+    }
     return relatedArtists;
 };
 
