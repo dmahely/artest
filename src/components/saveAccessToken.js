753608a8fcbfa6bfce28cd9054d0c98ce89a1912
Doaa Mahely <dmahely@hotmail.com>
2020-09-19 01:08:50 +0400

    Fix token destructuring syntax

--- src/components/saveAccessToken.js
+++ src/components/saveAccessToken.js
@@ -1,10 +1,7 @@
 const saveAccessToken = (tokenObject) => {
-    const [token, expires_at] = tokenObject;
+    const { token, expires_at } = tokenObject;
     localStorage.setItem('token', JSON.stringify(token));
-    localStorage.setItem(
-        'expiry',
-        JSON.stringify(expires_at)
-    );
-}
+    localStorage.setItem('expiry', JSON.stringify(expires_at));
+};
 
 export { saveAccessToken };
