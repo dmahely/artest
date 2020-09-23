856d86a996a8a390fbe038d8cdc5cc955589ce57
Doaa Mahely <dmahely@hotmail.com>
2020-09-23 22:27:37 +0400

    Remove unnecessary variable in fetchArtistData

--- src/components/fetchArtistData.js
+++ src/components/fetchArtistData.js
@@ -1,14 +1,12 @@
+// gets the correct artist ids for the upcoming rounds and fetches their data
+
 const baseURL = process.env.REACT_APP_SPOTIFY_BASE_URL;
 
-const fetchArtistData = async (albums, currentRound) => {
+const fetchArtistData = async (albums) => {
     const accessToken = JSON.parse(localStorage.getItem('token'));
 
     // get artist ids in a comma separated string
-    const artistIds = albums
-        .map((round) => {
-            return round.artists[currentRound - 1].id;
-        })
-        .join(',');
+    const artistIds = albums.map((round) => round.artists[0].id).join(',');
 
     const queryParams = new URLSearchParams({
         ids: artistIds,
@@ -22,7 +20,7 @@ const fetchArtistData = async (albums, currentRound) => {
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + accessToken,
+            Authorization: `Bearer ${accessToken}`,
         },
     });
 
