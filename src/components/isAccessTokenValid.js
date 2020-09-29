8b8e6fcecf395ba9422b087ebae4307be9a56d06
Doaa Mahely <dmahely@hotmail.com>
2020-09-29 23:49:22 +0400

    Remove unnecessary token call

--- src/components/isAccessTokenValid.js
+++ src/components/isAccessTokenValid.js
@@ -1,7 +1,5 @@
 // checks if an access token is valid by checking its expiry date against the current time
 // returns true if more than 5 minutes are left for the access token
-import { fetchAccessToken } from './fetchAccessToken';
-
 const isAccessTokenValid = async () => {
     const currentTokenExpiry = Number(localStorage.getItem('expiry'));
     const currentTime = Date.now();
@@ -9,12 +7,6 @@ const isAccessTokenValid = async () => {
         (currentTokenExpiry - currentTime) / (1000 * 60);
 
     if (differenceInMinutes > 5) return true;
-    else {
-        const newToken = await fetchAccessToken();
-        localStorage.setItem('token', newToken.token);
-        localStorage.setItem('expiry', newToken.expires_at);
-
-        return false;
-    }
+    else return false;
 };
 export { isAccessTokenValid };
