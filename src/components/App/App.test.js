d694344add4091704095c505fbc2a49d16f90628
Tryggvi Gylfason <tryggvi@spotify.com>
2020-10-20 18:57:00 +0200

    add test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { App } from './App';
 
-test('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Guess which artist the album belongs to/i)).toBeVisible();
+it('renders learn react link', () => {
+    render(<App />);
+    expect(
+        screen.getByText(/Guess which artist the album belongs to/i)
+    ).toBeVisible();
+});
+
+it('goes to round selection when the start button is clicked', () => {
+    render(<App />);
+    const startButton = screen.getByRole('button', { name: /start/i });
+    fireEvent.click(startButton);
+    expect(
+        screen.getByText(/How many rounds do you want to play\?/i)
+    ).toBeVisible();
 });
