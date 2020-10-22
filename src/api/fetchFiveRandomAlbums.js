9b811472cf9d86651631f8445658b9bf4a1ecb67
Doaa Mahely <dmahely@hotmail.com>
2020-10-22 22:52:48 +0400

    Refactor fetchFiveRandomAlbums to call backend

--- src/api/fetchFiveRandomAlbums.js
+++ src/api/fetchFiveRandomAlbums.js
@@ -1,45 +1,16 @@
-import { wordBank } from '../utils/words';
-
-const baseURL = process.env.REACT_APP_SPOTIFY_BASE_URL;
-
 const fetchFiveRandomAlbums = async () => {
-    const randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     const accessToken = JSON.parse(localStorage.getItem('token'));
 
-    const queryParam = `%${randomWord}%`;
-    const offsetParam = 0; // to get the most relevant albums
-    const typeParam = 'album';
-    const limitParam = 5;
-
-    const queryParams = new URLSearchParams({
-        q: queryParam,
-        type: typeParam,
-        limit: limitParam,
-        offset: offsetParam,
-    });
-
-    const stringifiedQueryParams = queryParams.toString();
-
-    // append params to baseURL
-    const searchEndpoint = `${baseURL}/search?${stringifiedQueryParams}`;
-
-    const albumsResponse = await fetch(searchEndpoint, {
+    const albums = await fetch('/albums', {
+        method: 'POST',
         headers: {
-            Accept: 'application/json',
             'Content-Type': 'application/json',
-            Authorization: `Bearer ${accessToken}`,
         },
+        body: JSON.stringify({ accessToken }),
     });
+    const jsonified = await albums.json();
 
-    let albumsData;
-    try {
-        albumsData = await albumsResponse.json();
-    } catch (err) {
-        console.log(err);
-        throw new Error(err);
-    }
-
-    return albumsData;
+    return jsonified;
 };
 
 export { fetchFiveRandomAlbums };
