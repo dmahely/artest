6aef18a36a261532d756a7acd6bab7c0b9fc6d0f
Doaa Mahely <dmahely@hotmail.com>
2020-10-23 20:00:21 +0400

    Update prop name

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Progress.css';
 
-const Progress = ({ width, currentRound, rounds }) => {
+const Progress = ({ width, currentRound, roundsNum }) => {
     return (
         <div className="Progress--container">
             <span
                 className="Progress--percentage"
                 style={{ width: width + '%' }}
             >
-                {currentRound}/{rounds}
+                {currentRound}/{roundsNum}
             </span>
         </div>
     );
