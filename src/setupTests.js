e52f2e98b7067e6fcbf254178c2ce474f95950ac
Doaa Mahely <dmahely@hotmail.com>
2020-10-27 23:40:12 +0400

    Add local storage mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,6 +5,24 @@
 import '@testing-library/jest-dom/extend-expect';
 import { server } from './test/server';
 
+var localStorageMock = (function () {
+    var store = {};
+    return {
+        getItem: function (key) {
+            return store[key] || null;
+        },
+        setItem: function (key, value) {
+            if (value) {
+                store[key] = value.toString();
+            }
+        },
+        clear: function () {
+            store = {};
+        },
+    };
+})();
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
