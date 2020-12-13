bb4a9da8cc4492f6dc9f95aa2bce60ab99340047
Doaa Mahely <dmahely@hotmail.com>
2020-12-13 19:17:07 +0400

    Add more result descriptions

--- src/utils/resultText.js
+++ src/utils/resultText.js
@@ -1,7 +1,7 @@
 const resultText = {
     poor: ['Oops!', 'Better luck next time', "A toddler could've done better!"],
-    average: ['Well done'],
-    excellent: ['Awesome job!', 'Wonderful', 'Awesomesauce', 'Wow!'],
+    average: ['Well done', 'Good job', 'Commendable effort'],
+    excellent: ['Great job!', 'Wonderful', 'Awesomesauce', 'Wow!'],
 };
 
 export { resultText };
